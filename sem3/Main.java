bdeed65cb8b47e9c07e9fd8c5e2b52c326bb4090
Andrey Kisetov <rbcfff@yandex.ru>
2022-11-23 03:13:41 +0300

    added sem3

---
+++ sem3/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+ 
+        numbers.removeIf( number -> number%2 == 0 );
+         
+        System.out.println(numbers);
+    }
+}
